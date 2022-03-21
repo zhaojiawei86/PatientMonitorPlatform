bb0917c6773302984cec6f767a2f15db44eefda5
zhaojiawei86 <zjw642337320@163.com>
2022-03-21 13:57:55 -0400

    update device flask

--- modules/devices.py
+++ modules/devices.py
@@ -71,5 +71,51 @@ def single_device(device_id):
             return f"The device with id {device_id} has been deleted.", 200
 
 
+@app.route('/appointments', methods=['GET'])
+def appointments():
+    '''get appointments'''
+    with sqlite3.connect("../database/db.sqlite3") as conn:
+        if request.method == 'GET':
+            cursor = conn.execute("SELECT * FROM appointments")
+            appointments_list = [
+                dict(Appointment_ID=row[0], Doctor_ID=row[1], Patient_ID=row[2],
+                     Device_ID=row[3], Device_Output=row[4], Date=row[5], Alert=row[6])
+                for row in cursor.fetchall()
+            ]
+            if appointments_list is not None:
+                return jsonify(appointments_list)
+
+
+@app.route('/appointment/<int:apt_id>', methods=['GET', 'PUT'])
+def single_apt(apt_id):
+    '''get/ change/ delete device'''
+    with sqlite3.connect("../database/db.sqlite3") as conn:
+        cursor = conn.cursor()
+        apt = None
+        if request.method == 'GET':
+            cursor.execute(
+                "SELECT * FROM appointments WHERE Appointment_ID=?", (apt_id,))
+            rows = cursor.fetchall()
+            for row in rows:
+                apt = row
+            if apt is not None:
+                return jsonify(apt), 200
+            else:
+                return f"Cannot find appointment {apt_id}", 404
+
+        if request.method == 'PUT':
+            sql = """UPDATE appointments
+                    SET Device_ID=?
+                    WHERE Appointment_ID=?"""
+            device_id = request.form['Device_ID']
+            updated_apt = {
+                'Appointment_ID': apt_id,
+                'Device_ID': device_id
+            }
+            conn.execute(sql, (device_id, apt_id,))
+            conn.commit()
+            return jsonify(updated_apt)
+
+
 if __name__ == "__main__":
     app.run(debug=True)
